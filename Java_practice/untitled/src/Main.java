efdf1702fcca76112550044134e30fa1e95e38f4
gitCoding47 <goodlife6yr@gmail.com>
2024-07-05 21:07:34 +0900

    숫자 자동생성 연습

--- Java_practice/untitled/src/Main.java
+++ Java_practice/untitled/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("+++++++++++++++++++++++++++++++++++++++++++++");
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("1. 자동번호 생성 횟수: ");
+    int cycle = sc.nextInt();
+    int[] arr = new int[7];
+    int m = 0;
+
+    while (cycle > 0) {
+      for (int i = 0; i < arr.length; i++) {
+        arr[i] = (int) (Math.random() * 45) + 1;
+
+        for (int j = 0; j < i; j++) {
+          if (arr[i] == arr[j]) {
+            i--;
+          }
+        }
+      }
+      for (int  i =0; i < arr.length - 1; i++) {
+        System.out.printf("%5d", arr[i]);
+      }
+      System.out.printf("   +%5d", arr[6]);
+      System.out.println();
+      cycle--;
+    }
+    System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
   }
 }
