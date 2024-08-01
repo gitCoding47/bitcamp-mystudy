b7b8a3303cd8bffc93322e455dcaa1a2e69e859a
gitCoding47 <goodlife6yr@gmail.com>
2024-08-01 12:56:30 +0900

    .

--- Java_practice/untitled/src/Main.java
+++ Java_practice/untitled/src/Main.java
@@ -1,28 +1,6 @@
-import java.io.*;
-import java.util.HashMap;
-
 public class Main {
-  static class My {
-    String a;
-    public My(String a) {
-      this.a = a;
-    }
-  }
-
   public static void main(String[] args) {
-
-    HashMap<Integer, My> map = new HashMap<>();
-
-    for (int i = 0; i < 10; i++) {
-      My my = new My(String.valueOf(Math.random() * 10000));
-      System.out.println(my.a);
-      map.put(i + 1, my);
-    }
-
-    System.out.println("---------------------------------");
-
-    for (My my : map.values()) {
-      System.out.println(my.a);
-    }
+    System.out.printf("%o / %d = %f", 100, 200, 0.5F);
+    System.out.println();
   }
 }
