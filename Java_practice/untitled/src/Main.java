171ecc3f004e854775b6c09dbefaafd91d09b895
gitCoding47 <goodlife6yr@gmail.com>
2024-08-13 12:46:21 +0900

    업데이트

--- Java_practice/untitled/src/Main.java
+++ Java_practice/untitled/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.printf("%o / %d = %f", 100, 200, 0.5F);
-    System.out.println();
+    List <String> list = new ArrayList<>();
+
+    list.add("이협건");
+    list.add("홍길동");
+    list.add("임꺽정");
+    list.add("고길동");
+
+    int listSize = list.size();
+
+    System.out.println("저장된 데이터 수 : " + listSize);
+
+    for (int i = 0; i < listSize; i++) {
+      System.out.println(list.get(i));
+    }
+
   }
 }
